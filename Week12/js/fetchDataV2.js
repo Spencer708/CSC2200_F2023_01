d5eaade14d6f4e6c4d38df35866e2c83df9c5996
Spencer708 <spencermyka@gmail.com>
2023-12-04 13:28:36 -0600

    Dec.4

---
+++ Week12/js/fetchDataV2.js
@@ -0,0 +1,32 @@
+function loadTable(id){
+    let url=`https://jsonplaceholder.typicode.com/todos/${id}`
+    fetch(url)
+        .then(resp => {
+            if(!resp.ok){
+                throw new Error("HTTP ERROR Status" + resp.status);
+            }
+            return resp.json();
+        }).then(jObj => {
+            // console.log("Made it ->");
+            // console.log(jObj);
+            addToTable(jObj);
+        }).catch(error => {
+        console.log("Error ->");
+        console.log(error);
+    })
+    document.getElementById("result").innerHTML = "Did it work";
+}
+function addToTable(data) {
+    const tBody = document.getElementById("theBody");
+    for (let i = 0; i < data.length; i++) {
+        let row = data[i]
+        const newTR = document.createElement("tr");
+        newTR.innerHTML = `
+            <td> ${row.userId} </td>
+            <td> ${row.id} </td>
+            <td> ${row.title} </td>
+            <td> ${row.completed} </td>
+        `;
+        tBody.appendChild(newTR);
+    }
+}
